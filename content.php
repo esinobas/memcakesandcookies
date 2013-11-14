b2c1c867f3ce39697b9e09825cf3bac76037a004
Esteban Sinobas Carpio <tebi@Ubuntu-Webserver.(none)>
2013-11-14 21:18:27 +0100

    Start

---
+++ content.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+   <head><title>MEM Cakes &amp; Cookies</title>
+
+
+   <meta name="generator" content="Bluefish 2.2.4" >
+   <meta name="author" content="Esteban Sinobas Carpio" >
+   <meta name="date" content="2013-11-14T21:14:53+0100" >   
+   <meta name="copyright" content="">
+   <meta name="ROBOTS" content="INDEX, FOLLOW">
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+   <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+   <meta http-equiv="content-style-type" content="text/css">
+   <meta http-equiv="expires" content="0">
+   <meta name="Description" content="Tartas (cakes) decoradas con fondant. Galletas (cookies) decoradas con fondant. Tartas de fantasia modeladas con fondant. Tartas y galletas de fondant en Madrid. Cakes and cookies with fondant in Madrid. Tartas y galletas para cumpleaños, comuniones y todo tipo de eventos y celebraciones">
+   <meta name="keywords" content="memcakesandcookies, cakes, cookies, cake, cookie, cupcakes, cupcake, tartas, galletas, cumpleaños, celebraciones, eventos, catering, bodas, comuniones, dulces">
+
+   
+   </head>
+   <body>
+    Aqui comenzamos
+    
+   </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>NEW MEM Cakes & Cookies</title>
+<meta name="generator" content="Bluefish 2.2.3" >
+<meta name="author" content="Esteban Sinobas Carpio" >
+<meta name="date" content="2013-03-19T20:25:40+0100" >
+<meta name="copyright" content="">
+<meta name="Description" content="Tartas (cakes) decoradas con fondant. Galletas (cookies) decoradas con fondant. Tartas de fantasia modeladas con fondant. Tartas y galletas de fondant en Madrid. Cakes and cookies with fondant in Madrid. Tartas y galletas para cumpleaños, comuniones y todo tipo de eventos y celebraciones">
+<meta name="keywords" content="memcakesandcookies, cakes, cookies, cake, cookie, cupcakes, cupcake, tartas, galletas, cumpleaños, celebraciones, eventos, catering, bodas, comuniones, dulces">
+<meta name="ROBOTS" content="INDEX, FOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="expires" content="0">
+</head>
+<body>
+
+   <script>
+      location.href='content.php'; 
+   </script>
+</body>
+</html>
