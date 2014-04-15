b9fb77c1be7d2f0ab2ed31938cb0003ccf84cf70
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2014-04-15 16:37:50 +0200

    Add call to FileBrowserFactory from FileBrowser.php

--- plugins/FileBrowser/FileBrowser.php
+++ plugins/FileBrowser/FileBrowser.php
@@ -4,14 +4,35 @@
     */
 
    include_once ($_SERVER['DOCUMENT_ROOT'].'/php/log4php/Logger.php');
-   
+   include_once ($_SERVER['DOCUMENT_ROOT'].'/plugins/FileBrowser/FileBrowserDataIf.php');
+   include_once ($_SERVER['DOCUMENT_ROOT'].'/plugins/FileBrowser/FileBrowserFactory.php');
 
    Logger::configure($_SERVER['DOCUMENT_ROOT'].'/plugins/FileBrowser/LogConfig.xml');
    $logger = Logger::getLogger(__FILE__);
    
-   $path = $_POST["path"];
-   $filter = $_POST["filter"];
    
-   $logger->trace("Parameters received in post: path [ ".$path." ] filter [ ". $filter . "]");
+   $path = $_POST["path"];
+   $filter = "";
+   $params = "";
+   $type ="Directory";
+   
+   $logger->trace("Parameters received in post: path [ ".$path." ]");
+   if (isset($_POST["filter"])){
+      $filter = $_POST["filter"];
+      $logger->trace("Parameters receive in post: filter [ ".$filter. " ]");
+   }
+   if (isset($_POST["$params"])){
+      $params = $_POST["$params"];
+      $logger->trace("Parameters receive in post: $params [ ".$params. " ]");
+   }
+   
+   //sacar los parametros. Ha que sacar el type, que por defecto es Directory.
+   
+   $logger->trace("The type of the file browser is [ " .$type ." ]");
+   //Crear el jason con los parametros dependiendo del tipo
+   //para el tipo directory, con el path y es suficiente
+   //path->....
+   $fileBrowserDataIf = FileBrowserFactory::getFileBrowserData($type, $path);
    echo '/php/log4php/Logger.php';
+   
 ?>
--- plugins/FileBrowser/FileBrowserFactory.php
+++ plugins/FileBrowser/FileBrowserFactory.php
@@ -4,9 +4,9 @@
  * Class that creates a FileBrowserIf object (class) and it returns them.
  */
 
-include_once ('FileBrowserDataIf.php');
-//include_once ($_SERVER['DOCUMENT_ROOT'].'/php/log4php/Logger.php');
-include_once ('/home/tebi/Datos/webserver/MEMcakesandcookies/www/php/log4php/Logger.php');
+   include_once ('FileBrowserDataIf.php');
+   include_once ($_SERVER['DOCUMENT_ROOT'].'/php/log4php/Logger.php');
+
 
 class FileBrowserFactory{
    
@@ -22,9 +22,13 @@ class FileBrowserFactory{
    public static function getFileBrowserData($theType, $theParameters = NULL){
       
       
-       Logger::configure('LogConfig.xml');
+       Logger::configure($_SERVER['DOCUMENT_ROOT'].'/plugins/FileBrowser/LogConfig.xml');
        $logger = Logger::getLogger(__CLASS__);
        $logger->trace("ENTER");
+       
+       $logger->trace("Type of FileBrowser to be created [ " .$theType. " ]".
+                             " with this parameters [ " .$theParameters . " ]" );
+       
        $logger->trace("EXIT");
       
    }
