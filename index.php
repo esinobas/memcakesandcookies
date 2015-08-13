42502db9b4de2a2e60cbc810b409f0e5e86d65d3
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2015-08-13 16:50:09 +0200

    Start the modify of the index.php

--- index.php
+++ index.php
@@ -1,44 +1,58 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<head>
-<title>MEM Cakes & Cookies</title>
-<meta name="generator" content="Bluefish 2.2.4" >
-<meta name="author" content="Esteban Sinobas Carpio" >
-<meta name="date" content="2013-11-30T18:53:11+0100" >
-<meta name="copyright" content="">
-<meta name="Description" content="Tartas (cakes) decoradas con fondant. Galletas (cookies) decoradas con fondant. Tartas de fantasia modeladas con fondant. Tartas y galletas de fondant en Madrid. Cakes and cookies with fondant in Madrid. Tartas y galletas para cumpleaños, comuniones y todo tipo de eventos y celebraciones">
-<meta name="keywords" content="memcakesandcookies, cakes, cookies, cake, cookie, cupcakes, cupcake, tartas, galletas, cumpleaños, celebraciones, eventos, catering, bodas, comuniones, dulces">
-<meta name="ROBOTS" content="INDEX, FOLLOW">
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
-<meta http-equiv="content-style-type" content="text/css">
-<meta http-equiv="expires" content="0">
+   <head>
+   <title>MEM Cakes & Cookies</title>
+   <meta name="generator" content="Bluefish 2.2.4" >
+   <meta name="author" content="Esteban Sinobas Carpio" >
+   <meta name="date" content="2013-11-30T18:53:11+0100" >
+   <meta name="copyright" content="">
+   <meta name="Description" content="Tartas (cakes) decoradas con fondant. Galletas (cookies) decoradas con fondant. Tartas de fantasia modeladas con fondant. Tartas y galletas de fondant en Madrid. Cakes and cookies with fondant in Madrid. Tartas y galletas para cumpleaños, comuniones y todo tipo de eventos y celebraciones">
+   <meta name="keywords" content="memcakesandcookies, cakes, cookies, cake, cookie, cupcakes, cupcake, tartas, galletas, cumpleaños, celebraciones, eventos, catering, bodas, comuniones, dulces">
+   <meta name="ROBOTS" content="INDEX, FOLLOW">
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+   <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+   <meta http-equiv="content-style-type" content="text/css">
+   <meta http-equiv="expires" content="0">
 
 
-<!-- Scripts -->
-<script type="text/javascript" src="./scripts/jquery-1.9.0.js"></script>
-<script type="text/javascript" src="./scripts/SlideImages.js"></script>
-<script type="text/javascript" src="./plugins/Gallery/Gallery.js"></script>
-<script type="text/javascript" src="./plugins/Lightbox/Lightbox.js"></script>
+   <!-- Scripts -->
+   <script type="text/javascript" src="./scripts/jquery-1.9.0.js"></script>
+   <script type="text/javascript" src="./scripts/SlideImages.js"></script>
+   <script type="text/javascript" src="./plugins/Gallery/Gallery.js"></script>
+   <script type="text/javascript" src="./plugins/Lightbox/Lightbox.js"></script>
 
- <!-- Styles -->
+   <!-- Styles -->
    
-   <link rel="stylesheet" type="text/css" href="./style/StyleMain.css"> 
-   <link rel="stylesheet" type="text/css" href="./plugins/Gallery/Gallery.css"> 
-   <link rel="stylesheet" type="text/css" href="./plugins/Lightbox/Lightbox.css">
+      <link rel="stylesheet" type="text/css" href="./style/StyleMain.css"> 
+      <link rel="stylesheet" type="text/css" href="./plugins/Gallery/Gallery.css"> 
+      <link rel="stylesheet" type="text/css" href="./plugins/Lightbox/Lightbox.css">
    
    
-<?php
-  require_once(dirname(__FILE__).'/php/localConfiguration/configuration.php');
-?>
-</head>
-<body>
-
+   <?php
+      include_once dirname(__FILE__).'/php/LoggerMgr/LoggerMgr.php';
+      $logger =  LoggerMgr::Instance()->getLogger("index.php");
+      //require_once(dirname(__FILE__).'/php/localConfiguration/configuration.php');
+   ?>
+   </head>
+   <body>
+      <?php
+         
+         $logger->debug("A request has been received from [ ". $_SERVER['REMOTE_ADDR'] . " ]"); 
+         $logger->trace("Getting the configuration");
+         include_once dirname(__FILE__).'/php/database/TB_Configuration.php';
+         $tableConfiguration = new TB_Configuration();
+         $tableConfiguration->open();
+         $logger->trace("The configuration was get");
+      ?>
     <div id="Contenedor">
          <div id="Cabecera">
             
             <div id="Logo">
-               <a href=<?php printf("\"%s\"", url);?>>
+               <?php 
+                  $tableConfiguration->searchByKey("URL");
+                  $logger->trace("Get logo from \"". $tableConfiguration->getValue() . "\"");
+               ?> 
+               <a href=<?php printf("\"%s\"", $tableConfiguration->getValue());?>>
                   <img src="images/logoCMYKwithoutBorder.jpg" alt="MEM cakes and cookies" title="MEM cakes and cookies" >
                </a>
             </div>
@@ -108,6 +122,8 @@
          ?>
          </div>
       </div>
-    
-</body>
+       <?php
+         $logger->debug("The request was processed"); 
+       ?>
+   </body>
 </html>
