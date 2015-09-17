832152970d0c90145c35cd6274de99d4c92f16c6
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2015-09-17 15:12:37 +0200

    Show only the configuration used in the web

--- controlpanel/ControlpanelFunctions.php
+++ controlpanel/ControlpanelFunctions.php
@@ -17,18 +17,26 @@
       global $loggerCpF;
       $loggerCpF->trace("Enter");
       require_once 'database/TB_Configuration.php';
+      //Define the constanst that allows access to the data configuration
+      define(IMAGES_CAKES_DIRECTORY_C, 'cakesImagesPath');
+      define(IMAGES_COOKIES_DIRECTORY_C, 'cookiesImagesPath');
+      define(IMAGES_MODELS_DIRECTORY_C, 'modelsImagesPath');
+      define(NUM_THUMBNAILS_C, 'numberThumbnails');
+      define(THUMBNAILS_DIRECTORY_C, 'thumbnailsPath');
+      define(URL_C, 'URL');
+      
       $tbConfiguration = new TB_Configuration();
       $tbConfiguration->open();
+      
 ?>
       <div id="DataConfiguration" class="Data-Grid">
         
 <?php 
-      while ($tbConfiguration->next()){
-
+      $tbConfiguration->searchByKey(URL_C);
 ?>
          <div id="DataConfiguration-<?php print ($tbConfiguration->getProperty());?>" class="Data-Grid-Row">
             <div class="Data-Grid-Column">
-                  <?php printf ("%s: ", $tbConfiguration->getLabel());?>
+                 <?php printf ("%s: ", $tbConfiguration->getLabel());?>
             </div>
             <div class="Data-Grid-Column" title=<?php printf("\"%s\"", $tbConfiguration->getDescription());?>>
                <?php 
@@ -37,21 +45,130 @@
                            " ]");
                      
                ?>
-               <input type="<?php if(strcmp($tbConfiguration->getDataType(), "Numeric") == 0){print("number");}else{print("text");}?>"
-                      value="<?php print ($tbConfiguration->getValue());?>" 
-                      <?php if(strcmp($tbConfiguration->getDataType(), "Directory") == 0){print("readonly");} ?>>
+               <input type="text" 
+                      value="<?php print ($tbConfiguration->getValue());?>">
                
             </div>
-            
+            <div class="Data-Grid-Column">
+            </div>
+         </div>
+<?php
+      $tbConfiguration->rewind();
+      $tbConfiguration->searchByKey(IMAGES_CAKES_DIRECTORY_C);
+?>
+         <div id="DataConfiguration-<?php print ($tbConfiguration->getProperty());?>" class="Data-Grid-Row">
+            <div class="Data-Grid-Column">
+                 <?php printf ("%s: ", $tbConfiguration->getLabel());?>
+            </div>
+            <div class="Data-Grid-Column" title=<?php printf("\"%s\"", $tbConfiguration->getDescription());?> >
+               <?php 
+                     $loggerCpF->trace("The [ ".$tbConfiguration->getProperty().
+                            " ] type data is [ " . $tbConfiguration->getDataType() .
+                           " ]");
+                     
+               ?>
+               <input type="text" 
+                      value="<?php print ($tbConfiguration->getValue());?>" readonly>
+               
+            </div>
+            <div class="Data-Grid-Column">
+               Seleccionar
+            </div>
+         </div>
+<?php
+      $tbConfiguration->rewind();
+      $tbConfiguration->searchByKey(IMAGES_COOKIES_DIRECTORY_C);
+?>
+         <div id="DataConfiguration-<?php print ($tbConfiguration->getProperty());?>" class="Data-Grid-Row">
+            <div class="Data-Grid-Column">
+                 <?php printf ("%s: ", $tbConfiguration->getLabel());?>
+            </div>
+            <div class="Data-Grid-Column" title=<?php printf("\"%s\"", $tbConfiguration->getDescription());?> >
+               <?php 
+                     $loggerCpF->trace("The [ ".$tbConfiguration->getProperty().
+                            " ] type data is [ " . $tbConfiguration->getDataType() .
+                           " ]");
+                     
+               ?>
+               <input type="text" 
+                      value="<?php print ($tbConfiguration->getValue());?>" readonly>
+               
+            </div>
+            <div class="Data-Grid-Column">
+               Seleccionar
+            </div>
+         </div>
+<?php
+      $tbConfiguration->rewind();
+      $tbConfiguration->searchByKey(IMAGES_MODELS_DIRECTORY_C);
+?>
+         <div id="DataConfiguration-<?php print ($tbConfiguration->getProperty());?>" class="Data-Grid-Row">
+            <div class="Data-Grid-Column">
+                 <?php printf ("%s: ", $tbConfiguration->getLabel());?>
+            </div>
+            <div class="Data-Grid-Column" title=<?php printf("\"%s\"", $tbConfiguration->getDescription());?>>
+               <?php 
+                     $loggerCpF->trace("The [ ".$tbConfiguration->getProperty().
+                            " ] type data is [ " . $tbConfiguration->getDataType() .
+                           " ]");
+                     
+               ?>
+               <input type="text" 
+                      value="<?php print ($tbConfiguration->getValue());?>" readonly>
+               
+            </div>
+            <div class="Data-Grid-Column">
+               Seleccionar
+            </div>
+         </div>
+
+<?php
+      $tbConfiguration->rewind();
+      $tbConfiguration->searchByKey(THUMBNAILS_DIRECTORY_C);
+?>
+         <div id="DataConfiguration-<?php print ($tbConfiguration->getProperty());?>" class="Data-Grid-Row">
+            <div class="Data-Grid-Column">
+                 <?php printf ("%s: ", $tbConfiguration->getLabel());?>
+            </div>
+            <div class="Data-Grid-Column" title=<?php printf("\"%s\"", $tbConfiguration->getDescription());?>>
+               <?php 
+                     $loggerCpF->trace("The [ ".$tbConfiguration->getProperty().
+                            " ] type data is [ " . $tbConfiguration->getDataType() .
+                           " ]");
+                     
+               ?>
+               <input type="text" 
+                      value="<?php print ($tbConfiguration->getValue());?>">
+               
+            </div>
+
          </div>
          
-<?php 
-      }
+<?php
+      $tbConfiguration->rewind();
+      $tbConfiguration->searchByKey(NUM_THUMBNAILS_C);
 ?>
+         <div id="DataConfiguration-<?php print ($tbConfiguration->getProperty());?>" class="Data-Grid-Row">
+            <div class="Data-Grid-Column">
+                 <?php printf ("%s: ", $tbConfiguration->getLabel());?>
+            </div>
+            <div class="Data-Grid-Column" title=<?php printf("\"%s\"", $tbConfiguration->getDescription());?> >
+               <?php 
+                     $loggerCpF->trace("The [ ".$tbConfiguration->getProperty().
+                            " ] type data is [ " . $tbConfiguration->getDataType() .
+                           " ]");
+                     
+               ?>
+               <input type="number" 
+                      value="<?php print ($tbConfiguration->getValue());?>" style="width: 50px">
+               
+            </div>
+           
+         </div>
       </div>
       <script type="text/javascript">
-      DataGrid.format($('#DataConfiguration'),{width:"500px",
-                                               columnsWidth: {0:"200px",1:"300px"}});
+      DataGrid.format($('#DataConfiguration'),{width:"575px",
+                                               columnsWidth: {0:"175px",1:"300px",2:"100px"}});
       </script>
 
 
