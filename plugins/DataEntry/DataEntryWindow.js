2136d5d7481067c5b5a47a4d9b04352ec4097fd9
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2015-10-27 17:09:43 +0100

    Close the add image collection window when the cancel button is pushed

--- plugins/DataEntry/DataEntryWindow.js
+++ plugins/DataEntry/DataEntryWindow.js
@@ -8,6 +8,20 @@ DataEntryWindow.PARAMETER_SIZE_C = "size";
 DataEntryWindow.PARAMETER_SIZE_WIDTH_C = "width";
 DataEntryWindow.PARAMETER_SIZE_HEIGHT_C = "height";
 
+
+/**
+ * Private funtion to close the Window
+ */
+var closeWindow = function(theIdHtmlObject){
+   
+   JSLogger.getInstance().registerLogger("DataEntryWindow", JSLogger.levelsE.TRACE);
+   JSLogger.getInstance().traceEnter();
+   JSLogger.getInstance().trace("Close window [ " + theIdHtmlObject +" ]");
+   $(theIdHtmlObject).addClass('DataEntryWindow-Hide');
+   $(".DataEntryBackground").remove();
+   JSLogger.getInstance().traceExit();
+}
+
 DataEntryWindow.show = function (theIdHtmlObject, theCallback, theOptionalParameters){
    
    JSLogger.getInstance().registerLogger("DataEntryWindow", JSLogger.levelsE.TRACE);
@@ -33,8 +47,15 @@ DataEntryWindow.show = function (theIdHtmlObject, theCallback, theOptionalParame
           $(theIdHtmlObject).css('margin-top',
             (0-parseInt(theOptionalParameters[DataEntryWindow.PARAMETER_SIZE_C][DataEntryWindow.PARAMETER_SIZE_HEIGHT_C])/2)+"px");
    }
+   JSLogger.getInstance().trace("Add the close functionality to the cancel button");
+   $(theIdHtmlObject).find(".DataEntryWindowButtonCancel").click(function(){
+         closeWindow(theIdHtmlObject);
+       }
+   );
+   
    JSLogger.getInstance().trace("Add the window background-shadow");
    $(theIdHtmlObject).before('<div class="DataEntryBackground"></div>');
    $(theIdHtmlObject).removeClass('DataEntryWindow-Hide');
+   
    JSLogger.getInstance().traceExit();
 }
