36416adc724afa44bac7719d218f568e61a0d599
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2015-12-05 00:04:36 +0100

    Format the post in main page

--- PageFunctions.php
+++ PageFunctions.php
@@ -186,6 +186,20 @@
          SingletonHolder::getInstance()->getObject('Logger')->trace("Exit");
          return $date;
       }
+      
+      static private function getPlainTextIntroFromHtml($html, $numchars) {
+         // Remove the HTML tags
+         $html = strip_tags($html);
+         // Convert HTML entities to single characters
+         $html = html_entity_decode($html, ENT_QUOTES, 'UTF-8');
+         // Make the string the desired number of characters
+         // Note that substr is not good as it counts by bytes and not characters
+         $html = mb_substr($html, 0, $numchars, 'UTF-8');
+         // Add an elipsis
+         $html .= "…";
+         return $html;
+      }
+      
       /**
        * Writes the blog or posts section
        */
@@ -230,7 +244,7 @@
                            </div>
                         </div>
                         <div class="Post-Begin">
-                           <?php print($tbPost->getNew());?>
+                           <?php print(self::getPlainTextIntroFromHtml($tbPost->getNew(), 200));?>
                         </div>
                         
                         <div class="Post-Read">
