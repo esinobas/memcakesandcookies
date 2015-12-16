f8a8a882259fd85e69f159902dc13e5293b5a384
Esteban Sinobas Carpio <esteban.sinobas@gmail.com>
2015-12-16 11:13:47 +0100

    Get info from previous and next image in the gallery

--- plugins/Gallery/Gallery.js
+++ plugins/Gallery/Gallery.js
@@ -16,7 +16,7 @@ var Gallery = Gallery || {};
  */
 Gallery.init = function(theHtmlObjectId, theLoadImage){
 
-   JSLogger.getInstance().registerLogger("Gallery", JSLogger.levelsE.WARN);
+   JSLogger.getInstance().registerLogger("Gallery", JSLogger.levelsE.TRACE);
    JSLogger.getInstance().traceEnter();
    JSLogger.getInstance().trace("Init gallery for the object [ " + theHtmlObjectId + " ]");
    var numGalleryImages = $('#'+theHtmlObjectId+' img').length;
@@ -47,8 +47,46 @@ Gallery.init = function(theHtmlObjectId, theLoadImage){
          var nextImage = (idx < numGalleryImages -1 );
      
          $(this).addClass('Gallery-Item');
+         
          $(this).click(function(){
             
+            var objPreviousImage = null;
+            var objNextImage = null;
+            
+            if (prevImage){
+               objPreviousImage = {};
+               if (typeof ($($('#'+theHtmlObjectId+' img').get(idx-1)).attr('data-image_source')) !== 'undefined'){
+                  objPreviousImage.src = $($('#'+theHtmlObjectId+' img').get(idx-1)).attr('data-image_source');
+               }else{
+                  objPreviousImage.src = $($('#'+theHtmlObjectId+' img').get(idx-1)).attr('src');
+               }
+               if (typeof $($('#'+theHtmlObjectId+' img').get(idx-1)).attr('data-image_desc') !== 'undefined'){
+                  objPreviousImage.desc = $($('#'+theHtmlObjectId+' img').get(idx-1)).attr('data-image_desc');
+               }else{
+                  objPreviousImage.desc = $($('#'+theHtmlObjectId+' img').get(idx-1)).attr('title');
+               }
+               
+               JSLogger.getInstance().trace("The previous image source [ " +
+                     objPreviousImage.src  + 
+                     " ] & previous image desc [ " + objPreviousImage.desc + " ]");
+            }
+            if (nextImage){
+               objNextImage = {};
+               if (typeof ($($('#'+theHtmlObjectId+' img').get(idx+1)).attr('data-image_source')) !== 'undefined'){
+                  objNextImage.src = $($('#'+theHtmlObjectId+' img').get(idx+1)).attr('data-image_source');
+               }else{
+                  objNextImage.src = $($('#'+theHtmlObjectId+' img').get(idx+1)).attr('src');
+               }
+               if (typeof $($('#'+theHtmlObjectId+' img').get(idx+1)).attr('data-image_desc') !== 'undefined'){
+                  objNextImage.desc = $($('#'+theHtmlObjectId+' img').get(idx+1)).attr('data-image_desc');
+               }else{
+                  objNextImage.desc = $($('#'+theHtmlObjectId+' img').get(idx+1)).attr('title');
+               }
+               
+               JSLogger.getInstance().trace("The next image source [ " +
+                     objNextImage.src  + 
+                     " ] & next image desc [ " + objNextImage.desc + " ]");
+            }
             Lightbox.show(objImage, theLoadImage);
          });
       });
